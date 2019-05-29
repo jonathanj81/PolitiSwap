631bade74daf67d5dd0ada1ad20227bb1a631d9c
Jon Johnson <jondavidjohnson81@gmail.com>
2019-05-29 09:26:15 -0400

    Pause to push commit

---
+++ app/src/main/java/com/politiswap/politiswap/NewVersion/SnapByOne.java
@@ -0,0 +1,31 @@
+package com.politiswap.politiswap.NewVersion;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.LinearSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SnapByOne extends LinearSnapHelper {
+
+    @Override
+    public int findTargetSnapPosition(RecyclerView.LayoutManager layoutManager, int velocityX, int velocityY) {
+
+        if (!(layoutManager instanceof RecyclerView.SmoothScroller.ScrollVectorProvider)) {
+            return RecyclerView.NO_POSITION;
+        }
+
+        final View currentView = findSnapView(layoutManager);
+
+        if (currentView == null) {
+            return RecyclerView.NO_POSITION;
+        }
+
+        final int currentPosition = layoutManager.getPosition(currentView);
+
+        if (currentPosition == RecyclerView.NO_POSITION) {
+            return RecyclerView.NO_POSITION;
+        }
+
+        return currentPosition;
+    }
+}
